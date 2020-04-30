4a503f8df1784685a3b92a6f36d53830a407e77e
Prakash <kuruvaprakash041@gmail.com>
2020-04-30 10:41:48 +0530

    adding all file together

---
+++ EvenNumber.java
@@ -0,0 +1,15 @@
+public class EvenNumber
+{
+ public static void main(String[] args) 
+ {
+ 	int n=30;
+ 	if (n%2==0 && n%3==0 && n%5==0) 
+ 	{
+ 		System.out.println(n+ "is a EvenNumber");
+ 	}
+ 	else
+    {
+ 	    System.out.println(n+ "is not an EvenNumber");	
+ 	}
+ }
+}
---
+++ NumPattern3.java
@@ -0,0 +1,20 @@
+class NumPattern3
+{
+ public static void main(String[] args) 
+ {
+    for (int i=1;i<=3;i++ ) 
+    {
+      for (int j=0;j<=1;j++ ) 
+        {
+    		System.out.print("$");
+            for (int k=0;k<=1;++k ) 
+            {
+                 System.out.print("#");
+                
+            }
+           
+    	}	
+    	System.out.println();
+    }
+ }
+}
